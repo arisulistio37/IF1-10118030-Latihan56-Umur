81eccd388233e5b7df87cab2d97d11622a5c80ec
arisulistio37 <arisulistio37@gmail.com>
2019-11-07 19:51:47 +0700

    Initial commit

---
+++ src/if1/pkg10118030/latihan56/umur/BarangAntik.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118030.latihan56.umur;
+
+/**
+ *
+ * @author Ari Sulistio
+ */
+public class BarangAntik {
+    int umur;
+    
+    public BarangAntik (int umur){
+        this.umur = umur;
+    }
+    
+    public void tampilUmur (){
+        System.out.println("Umur barang Antik ini adalah : "+umur+" tahun");
+    }
+}
---
+++ src/if1/pkg10118030/latihan56/umur/IF110118030Latihan56Umur.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118030.latihan56.umur;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ari Sulistio
+ * KELAS    : IF-1
+ * NIM      : 10118030
+ * Deskripsi program : program ini berisi tentang tampilan umur barang antik
+ */
+public class IF110118030Latihan56Umur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Radio radio = new Radio(234);
+        radio.setName("Radio");
+        System.out.println("Nama barang Antik : "+radio.getName());
+        radio.tampilUmur();
+    }
+    
+}
